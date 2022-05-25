4cbc44e6e168121d3a3562dc9c3c1712f12e6f91
Alexandr <acapovsky@gmail.com>
2022-05-25 11:22:53 +0200

    first commit

---
+++ src/main/kotlin/com/acapovsky/KotlinReactTest/KotlinReactTestApplication.kt
@@ -0,0 +1,11 @@
+package com.acapovsky.KotlinReactTest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinReactTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinReactTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/acapovsky/KotlinReactTest/KotlinReactTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.acapovsky.KotlinReactTest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinReactTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
